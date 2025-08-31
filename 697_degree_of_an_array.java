bfd230c18b165979ae888ad7628e5edc881fb0d4
muskaann13 <muskanguptamg013@gmail.com>
2025-08-31 14:49:52 +0530

    Add 697_degree_of_an_array.java - LeetCode Problem 697

---
+++ 697_degree_of_an_array.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int findShortestSubArray(int[] nums) {
+        Map<Integer,Integer> freq = new HashMap<>();
+        Map<Integer,Integer> first = new HashMap<>();
+        Map<Integer,Integer> last = new HashMap<>();
+
+        for(int i=0; i<nums.length; i++) {
+            int num = nums[i];
+
+            freq.put(num, freq.getOrDefault(num, 0)+1);
+
+            if (!first.containsKey(num)) {
+                first.put(num, i);
+            }
+
+            last.put(num, i);
+        }
+
+        int degree = Integer.MIN_VALUE;
+        for(Map.Entry<Integer,Integer> entry : freq.entrySet()) {
+            degree = Math.max(degree, entry.getValue());
+        }
+
+        int minLen = Integer.MAX_VALUE;
+        for(int num : freq.keySet()) {
+            if (freq.get(num) == degree) {
+                int len = last.get(num) - first.get(num) + 1;
+                minLen = Math.min(minLen, len);
+            }
+        }
+        return minLen;
+    }
+}
