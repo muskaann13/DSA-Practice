301c27cb20cd7150a383060bb1f8d7b1b2d66416
muskaann13 <muskanguptamg013@gmail.com>
2025-08-31 18:22:17 +0530

    Add 372_super_pow.java - LeetCode Problem 372

---
+++ 372_super_pow.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int superPow(int a, int[] b) {
+        int n = b.length-1;
+        long p = 0;
+
+        for(int num : b) {
+            p += num*Math.pow(10, n--);
+        }
+
+        return helper(a, p);
+    }
+
+    private int helper(int a, long p) {
+        if (p == 1) {
+            return a;
+        }
+
+        int halfPower = helper(a, p/2);
+        int halfPowerSq = halfPower * halfPower;
+
+        if (p % 2 != 0) {
+            halfPowerSq *= a;
+        }
+        return halfPowerSq;
+    }
+}
