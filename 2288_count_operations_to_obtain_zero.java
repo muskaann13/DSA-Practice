1c3c5a48014299609b7de1b87f9831b8ed68f297
muskaann13 <muskanguptamg013@gmail.com>
2025-09-02 09:03:37 +0530

    Add 2288_count_operations_to_obtain_zero.java - LeetCode Problem 2288

---
+++ 2288_count_operations_to_obtain_zero.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int countOperations(int num1, int num2) {
+        int steps = 0;
+
+        return helper(num1, num2, steps);
+    }
+
+    public int helper(int num1, int num2, int steps) {
+        if (num1 == 0 || num2 == 0) {
+            return steps;
+        }
+
+        if (num1 <= num2) {
+            return helper(num1, num2-num1, steps+1);
+        } 
+        return helper(num1-num2, num2, steps+1);
+    }
+}
