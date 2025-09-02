b2b6fdd5c9c436109b5c847f8e41342946750459
muskaann13 <muskanguptamg013@gmail.com>
2025-09-02 08:29:04 +0530

    Update 1444_number_of_steps_to_reduce_a_number_to_zero.java - LeetCode Problem 1444

--- 1444_number_of_steps_to_reduce_a_number_to_zero.java
+++ 1444_number_of_steps_to_reduce_a_number_to_zero.java
@@ -1,9 +1,14 @@
 class Solution {
     public int numberOfSteps(int num) {
+        int steps = 0;
+        return helper(num, steps);
+    }
+
+    private int helper(int num, int steps) {
         if (num == 0) {
-            return 0;
+            return steps;
         }
 
-        return 1 + ((num%2==0) ? numberOfSteps(num/2) : numberOfSteps(num-1));
+        return (num%2 == 0) ? helper(num/2, steps+1) : helper(num-1, steps+1);
     }
 }
