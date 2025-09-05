f4b6578a51314e3835d7e66b7a20b14f3362faa1
muskaann13 <muskanguptamg013@gmail.com>
2025-09-05 22:12:34 +0530

    Add 31_next_permutation.java - LeetCode Problem 31

---
+++ 31_next_permutation.java
@@ -0,0 +1,51 @@
+// EASY EXPLANATION IN JUST 4 steps
+
+// 1) Find the Pivot:
+// Iterate from the end to find the first element that is smaller than the next element (nums[i] < nums[i + 1]).
+
+// 2) Find the Successor:
+// From the end, find the first element that is greater than nums[piv] (nums[j] > nums[piv]).
+
+// 3) Swap
+// Swap nums[piv] and nums[j].
+
+// 4) Reverse the Tail:
+// Reverse the subarray from i + 1 to the end to get the next smallest lexicographic permutation.
+
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int piv = -1;
+
+        for(int i=nums.length-2; i>=0; i--) {
+            if (nums[i] < nums[i+1]) {
+                piv = i;
+                break;
+            }
+        }
+
+        if (piv == -1) {
+            Arrays.sort(nums);
+            return;
+        }
+
+        for (int i=nums.length-1; i>piv; i--) {
+            if (nums[i] > nums[piv]) {
+                swap (nums, i, piv);
+            }
+        }
+
+        int left = piv+1;
+        int right = nums.length-1;
+        while (left <= right) {
+            swap(nums, left, right);
+            left++;
+            right--;
+        }
+    }
+
+    private void swap (int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
