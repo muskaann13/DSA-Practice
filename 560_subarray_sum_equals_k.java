6980a8c9e2dd0b2aaa55678c036c91c52deb24ef
muskaann13 <muskanguptamg013@gmail.com>
2025-09-29 04:47:05 +0530

    Update 560_subarray_sum_equals_k.java - LeetCode Problem 560

--- 560_subarray_sum_equals_k.java
+++ 560_subarray_sum_equals_k.java
@@ -1,15 +1,22 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
+        HashMap<Integer, Integer> prefixSumCount = new HashMap<>();
+        int currSum = 0;
         int count = 0;
 
+        prefixSumCount.put(0, 1);
+
         for (int i=0; i<nums.length; i++) {
-            int sum = 0;
-            for (int j=i; j<nums.length; j++) {
-                sum += nums[j];
-                if (sum == k) {
-                    ++count;
-                }
+            int num = nums[i];
+            currSum += num;
+
+            int targetSum = currSum - k;
+
+            if (prefixSumCount.containsKey(targetSum)) {
+                count += prefixSumCount.get(targetSum);
             }
+
+            prefixSumCount.put(currSum, prefixSumCount.getOrDefault(currSum, 0)+1);
         }
         return count;
     }
