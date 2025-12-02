6b3dfca10f472f2cbbc662661dec9a5c79ccfbb1
muskaann13 <muskanguptamg013@gmail.com>
2025-12-03 02:10:38 +0530

    Update 155_min_stack.java - LeetCode Problem 155

--- 155_min_stack.java
+++ 155_min_stack.java
@@ -1,27 +1,42 @@
 class MinStack {
-    int min = Integer.MAX_VALUE;
-    Stack<Integer> stack = new Stack<Integer>();
-    public void push(int x) {
-        // only push the old minimum value when the current 
-        // minimum value changes after pushing the new value x
-        if(x <= min){          
-            stack.push(min);
-            min=x;
+
+    Stack<Integer> mainStack;
+    Stack<Integer> minStack;
+
+    public MinStack() {
+        mainStack = new Stack<>();
+        minStack = new Stack<>();
+    }
+    
+    public void push(int val) {
+        mainStack.push(val);
+
+        if (minStack.isEmpty()) {
+            minStack.push(val);
+        } else {
+            minStack.push(Math.min(val, minStack.peek()));
         }
-        stack.push(x);
     }
-
+    
     public void pop() {
-        // if pop operation could result in the changing of the current minimum value, 
-        // pop twice and change the current minimum value to the last minimum value.
-        if(stack.pop() == min) min=stack.pop();
+        mainStack.pop();
+        minStack.pop();
     }
-
+    
     public int top() {
-        return stack.peek();
+        return mainStack.peek();
     }
-
+    
     public int getMin() {
-        return min;
+        return minStack.peek();
     }
 }
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
